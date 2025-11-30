9cdf50b0dd5d8f2e71674a56e8ce3a09c81de285
Nick Wallingford <nick@wallingford.cc>
2025-11-30 11:30:16 -0800

    prepare 2025

---
+++ src/2025/main.cpp
@@ -0,0 +1,44 @@
+void aoc_2025_1();
+void aoc_2025_2();
+void aoc_2025_3();
+void aoc_2025_4();
+void aoc_2025_5();
+void aoc_2025_6();
+void aoc_2025_7();
+void aoc_2025_8();
+void aoc_2025_9();
+void aoc_2025_10();
+void aoc_2025(int n) {
+  switch (n) {
+  case 1:
+    aoc_2025_1();
+    break;
+  case 2:
+    aoc_2025_2();
+    break;
+  case 3:
+    aoc_2025_3();
+    break;
+  case 4:
+    aoc_2025_4();
+    break;
+  case 5:
+    aoc_2025_5();
+    break;
+  case 6:
+    aoc_2025_6();
+    break;
+  case 7:
+    aoc_2025_7();
+    break;
+  case 8:
+    aoc_2025_8();
+    break;
+  case 9:
+    aoc_2025_9();
+    break;
+  case 10:
+    aoc_2025_10();
+    break;
+  }
+}
