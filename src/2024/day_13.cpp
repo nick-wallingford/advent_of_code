e36fbde6f87f33f63873236e2c536fc67c14ff73
Nick Wallingford <nick@wallingford.cc>
2025-11-30 10:27:55 -0800

    port days 13 and 14 from c

--- src/2024/day_13.cpp
+++ src/2024/day_13.cpp
@@ -1,1 +1,43 @@
-void aoc_2024_13() {}
+#include "util.hpp"
+#include <cstdint>
+#include <cstdio>
+
+void aoc_2024_13() {
+  int64_t a, b, c, d, y1, y2;
+  int64_t cost = 0;
+  const auto filename = get_filename(2024, 13);
+  FILE *file = fopen(filename.c_str(), "r");
+
+  // input is formatted thusly:
+  // a  c
+  // b  d
+  // y1 y2
+  while (6 == fscanf(file,
+                     "Button A: X%ld, Y%ld\n"
+                     "Button B: X%ld, Y%ld\n"
+                     "Prize: X=%ld, Y=%ld\n",
+                     &a, &c, &b, &d, &y1, &y2)) {
+#if 1
+    y1 += 10000000000000ll;
+    y2 += 10000000000000ll;
+#endif
+
+    // we solve the following linear equation:
+    // [ a b | x1 ] = [ y1 ]
+    // [ c d | x2 ] = [ y2 ]
+
+    // [ x1 ] = [  d -b ] [ y1 ]
+    // [ x2 ] = [ -c  a ] [ y2 ]
+    //          ----------------
+    //                det
+    const int64_t det = a * d - b * c;
+    int64_t x1 = d * y1 - b * y2;
+    int64_t x2 = a * y2 - c * y1;
+
+    if (!(x1 % det) && (x1 /= det) >= 0 && !(x2 % det) && (x2 /= det) >= 0)
+      cost += 3 * x1 + x2;
+  }
+
+  fclose(file);
+  printf("cost: %ld\n", cost);
+}
--- src/2024/day_14.cpp
+++ src/2024/day_14.cpp
@@ -1,1 +1,99 @@
-void aoc_2024_14() {}
+#include "util.hpp"
+#include <cinttypes>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+
+static constexpr int64_t width = 101;
+static constexpr int64_t height = 103;
+static constexpr int64_t duration = 40000;
+
+namespace {
+void part1() {
+  int64_t x, y, dx, dy;
+  int64_t quad[4] = {0, 0, 0, 0};
+  static constexpr int64_t mid_x = width >> 1;
+  static constexpr int64_t mid_y = height >> 1;
+  const auto file = fopen(get_filename(2024, 14).c_str(), "r");
+
+  while (4 == fscanf(file, "p=%" PRId64 ",%" PRId64 " v=%" PRId64 ",%" PRId64 "\n", &x, &y, &dx, &dy)) {
+    x += duration * dx;
+    x %= width;
+    if (x < 0)
+      x += width;
+    y += duration * dy;
+    y %= height;
+    if (y < 0)
+      y += height;
+
+    if (x < mid_x) {
+      if (y < mid_y)
+        quad[0]++;
+      else if (y > mid_y)
+        quad[1]++;
+    } else if (x > mid_x) {
+      if (y < mid_y)
+        quad[2]++;
+      else if (y > mid_y)
+        quad[3]++;
+    }
+  }
+
+  fclose(file);
+  printf("safety factor: %" PRId64 "\n", quad[0] * quad[1] * quad[2] * quad[3]);
+}
+
+void part2() {
+  FILE *file = fopen(get_filename(2024, 14).c_str(), "r");
+  int64_t x, y, dx, dy;
+
+  int64_t n = 0;
+  int64_t sum_x[duration];
+  int64_t sum_sqr_x[duration];
+  int64_t sum_y[duration];
+  int64_t sum_sqr_y[duration];
+  memset(sum_x, 0, sizeof(sum_x));
+  memset(sum_sqr_x, 0, sizeof(sum_sqr_x));
+  memset(sum_y, 0, sizeof(sum_y));
+  memset(sum_sqr_y, 0, sizeof(sum_sqr_y));
+
+  while (4 == fscanf(file, "p=%" PRId64 ",%" PRId64 " v=%" PRId64 ",%" PRId64 "\n", &x, &y, &dx, &dy)) {
+    ++n;
+    for (int64_t i = 0; i < duration; i++) {
+      sum_x[i] += x;
+      sum_y[i] += y;
+      sum_sqr_x[i] += x * x;
+      sum_sqr_y[i] += y * y;
+
+      x += dx;
+      while (x < 0)
+        x += width;
+      while (x >= width)
+        x -= width;
+      y += dy;
+      while (y < 0)
+        y += height;
+      while (y >= height)
+        y -= height;
+    }
+  }
+
+  fclose(file);
+
+  const double N = 1.0 / n;
+  const double N2 = 1.0 / (n * n);
+  for (int64_t i = 0; i < duration; i++) {
+    const double stddev_x = sqrt(sum_sqr_x[i] * N - sum_x[i] * sum_x[i] * N2);
+    const double stddev_y = sqrt(sum_sqr_y[i] * N - sum_y[i] * sum_y[i] * N2);
+    if (stddev_x + stddev_y < 40)
+      printf("%ld\t%16f\t%16f\n", i, stddev_x, stddev_y);
+  }
+}
+
+} // namespace
+
+void aoc_2024_14() {
+  part1();
+  part2();
+}
