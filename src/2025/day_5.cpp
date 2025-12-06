84e989bf1816174af7a20d224903552f4d5dd608
Nick Wallingford <nick@wallingford.cc>
2025-12-05 17:08:07 -0800

    improve day 5

--- src/2025/day_5.cpp
+++ src/2025/day_5.cpp
@@ -5,11 +5,9 @@
 
 void aoc_2025_5() {
   std::ifstream input{get_filename(2025, 5)};
-  size_t part1{}, part2{};
 
+  // read fresh ingredient ranges
   std::vector<std::pair<uint64_t, uint64_t>> fresh;
-  std::vector<uint64_t> produce;
-
   std::regex regex{"([0-9]+)-([0-9]+)"};
   for (std::string line; std::getline(input, line);) {
     std::smatch results;
@@ -18,38 +16,43 @@ void aoc_2025_5() {
 
     fresh.emplace_back(std::stoul(results[1].str()), std::stoul(results[2].str()));
   }
+  std::sort(fresh.begin(), fresh.end());
 
+  // read produce ids
+  std::vector<uint64_t> produce;
   for (std::string line; std::getline(input, line);)
     produce.emplace_back(std::stoul(line));
-
-  std::sort(fresh.begin(), fresh.end());
   std::sort(produce.begin(), produce.end());
 
+  // count part 1, do most of part 2.
+  size_t part1{}, part2{}, left{}, right{};
   auto f = fresh.cbegin();
   auto p = produce.cbegin();
-
   while (f != fresh.cend() && p != produce.cend()) {
     if (*p < f->first)
       ++p;
-    else if (*p > f->second)
+    else if (*p > f->second) {
+      if (const auto [a, b] = *f; a > right + 1) {
+        part2 += 1 + right - left;
+        left = a;
+        right = b;
+      } else
+        right = std::max(right, b);
       ++f;
-    else {
+    } else {
       ++part1;
       ++p;
     }
   }
 
-  size_t left{}, right{};
-  for (const auto [a, b] : fresh) {
-    if (a > right) {
+  // finish part 2. this should be nearly complete.
+  for (; f != fresh.cend(); ++f)
+    if (const auto [a, b] = *f; a > right + 1) {
       part2 += 1 + right - left;
       left = a;
       right = b;
-    } else {
+    } else
       right = std::max(right, b);
-    }
-  }
   part2 += right - left;
-
   std::cout << "part1: " << part1 << "\npart2: " << part2 << '\n';
 }
