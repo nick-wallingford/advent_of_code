86439cd5148ed765dff3e1f148d70ab2c37fa160
Nick Wallingford <nick@wallingford.cc>
2025-12-07 14:25:48 -0800

    simplify day 7

--- src/2025/day_7.cpp
+++ src/2025/day_7.cpp
@@ -29,37 +29,23 @@ void aoc_2025_7() {
   std::istringstream input{data};
 #endif
 
-  int64_t part1{}, part2{};
-  std::pair<std::vector<int64_t>, std::vector<int64_t>> worlds;
-
   std::string line;
   std::getline(input, line);
-
-  worlds.first.resize(line.size());
-  worlds.second.resize(line.size());
+  std::vector<int64_t> worlds(line.size(), 0);
   for (size_t i = 0; i < line.size(); i++)
-    if (line[i] == 'S') {
-      worlds.first[i] = 1;
-      line[i] = '|';
-    }
+    if (line[i] == 'S')
+      worlds[i] = 1;
 
-  for (std::string last_line = line; std::getline(input, line); last_line = line,
-                   std::swap(worlds.first, worlds.second),
-                   std::memset(worlds.second.data(), 0, worlds.second.size() * sizeof(int64_t)))
-    for (size_t j = 0; j < line.size(); j++)
-      if (last_line[j] == '|')
-        if (line[j] == '^') {
+  int64_t part1 = 0;
+  while (std::getline(input, line))
+    for (size_t i = 0; i < line.size(); i++)
+      if (worlds[i])
+        if (line[i] == '^') {
           ++part1;
-          line[j - 1] = '|';
-          line[j + 1] = '|';
-
-          worlds.second[j - 1] += worlds.first[j];
-          worlds.second[j + 1] += worlds.first[j];
-        } else {
-          line[j] = '|';
-          worlds.second[j] += worlds.first[j];
+          worlds[i - 1] += worlds[i];
+          worlds[i + 1] += worlds[i];
+          worlds[i] = 0;
         }
 
-  part2 = std::reduce(worlds.first.begin(), worlds.first.end());
-  std::cout << "part 1: " << part1 << "\npart 2: " << part2 << '\n';
+  std::cout << "part 1: " << part1 << "\npart 2: " << std::reduce(worlds.begin(), worlds.end()) << '\n';
 }
